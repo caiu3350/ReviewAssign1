d83c5d6fd754a966b17f186dd4b58ee56e598d0d
caiu3350 <caiu3350@ISS2TWQKH2.scdsb.pri>
2020-09-15 11:41:46 -0400

    jango

---
+++ src/Momentum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Momentum {
+
+    public static void main(String[] args) {
+        double mass, velocity, momentum;
+        Scanner s = new Scanner(System.in);
+        //ask for data
+        System.out.print("Enter objects mass (in kg) >> ");
+        mass = s.nextDouble();
+        System.out.print("Enter objects velocity (in m/s) >> ");
+        velocity = s.nextDouble();
+        //calculate momentum
+        momentum = mass*velocity;
+        System.out.println(String.format("The object's momentum is %.2f", momentum));
+    }
+    
+}
---
+++ src/PizzaCost.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class PizzaCost {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
