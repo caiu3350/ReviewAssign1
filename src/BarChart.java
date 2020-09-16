8ad18db4da4355d1e29d2c4d4248ef7becd1d29a
caiu3350 <caiu3350@ISS2TWQKH2.scdsb.pri>
2020-09-16 09:59:43 -0400

    jango

--- src/BarChart.java
+++ src/BarChart.java
@@ -1,11 +1,24 @@
 import TurtleGraphics.*;
+import java.util.Scanner;
+import java.awt.Color;
 public class BarChart {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pen P = new StandardPen();
+        Scanner s = new Scanner(System.in);
+        
+    }
+    public static void makeBar(Pen p,int xloc, int yloc, int length, Color col) {
+        p.up();
+        p.move(xloc,yloc);
+        p.down();
+        p.setColor(col);
+        p.setDirection(90);
+        p.move(length);
+        p.up();
+        p.move(15);
+        p.down();
+        p.drawString(length+"");
     }
     
 }
--- src/PizzaCost.java
+++ src/PizzaCost.java
@@ -5,6 +5,22 @@ public class PizzaCost {
         Scanner s = new Scanner(System.in);
         final double labour = 1.5;
         final double heat = 0.75;
+        int top;
+        double sizecost,topcost,total,size;
+        System.out.print("Enter size of pizza (inches) > ");
+        size = s.nextDouble();
+        System.out.print("Enter number of toppings > ");
+        top = s.nextInt();
+        sizecost=size*0.50;
+        topcost=top*0.75;
+        total=labour+heat+sizecost+topcost;
+        System.out.println("You have ordered a "+size+" inch pizza with "+top+" toppings");
+        System.out.println(String.format("Heat Cost = %.2f",heat));
+        System.out.println(String.format("Labour Cost = %.2f",labour));
+        System.out.println(String.format("Toppings Cost = %.2f",topcost));
+        System.out.println(String.format("Base Ingrediants Cost = %.2f",sizecost));
+        System.out.println("__________________________________");
+        System.out.println(String.format("Total Cost = %.2f",total));
     }
     
 }
